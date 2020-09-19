0f960bfda88e179495fc2df2bf7bfd20cff9bef0
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2020-09-19 10:50:51 +0700

    MineSweeper-Game-jv

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] map = {
+                {"*",".","*",".","."},
+                {".", "*", ".", ".", "."},
+                {".", ".", ".", ".", "."},
+                {".", ".", ".", "*", "."},
+                {".", "*", ".", ".", "."},
+                {".", "", ".", ".", "*"}
+
+        };
+        final  int MAP_HEIGH = map.length;
+        final int MAP_WIDTH = map[0].length;
+
+        String[][] mapReport = new String[MAP_HEIGH][MAP_WIDTH];
+        for (int yOrdinate = 0; yOrdinate < MAP_HEIGH; yOrdinate++) {
+            for (int xOrdinate = 0;xOrdinate < map[0].length ; xOrdinate++) {
+                String curentCell = map[0][xOrdinate];
+                if (curentCell.equals("*")) {
+                    mapReport[0][xOrdinate] = "*";
+                }else {
+                    final int[][] NEIGHBOURS_ORDINATE = {
+                            {yOrdinate - 1,xOrdinate - 1},{yOrdinate - 1,xOrdinate},{yOrdinate - 1,xOrdinate + 1},
+                            {yOrdinate,xOrdinate - 1},{yOrdinate,xOrdinate + 1},
+                            {yOrdinate + 1,xOrdinate - 1},{yOrdinate + 1,xOrdinate},{yOrdinate + 1,xOrdinate + 1}
+                    };
+
+                    int minesAround = 0;
+                    for (int i = 0; i < NEIGHBOURS_ORDINATE.length; i++) {
+                        int[] neighbourOrdinate = NEIGHBOURS_ORDINATE[i];
+                        int xOrdinateOfNeighbour = neighbourOrdinate[1];
+                        int yOrdinateOfNeighbour = neighbourOrdinate[0];
+
+                        boolean isOutOfMapNeighbour = xOrdinateOfNeighbour < 0
+                                || xOrdinateOfNeighbour == MAP_WIDTH
+                                ||yOrdinateOfNeighbour < 0
+                                ||yOrdinateOfNeighbour == MAP_HEIGH;
+                        if (isOutOfMapNeighbour) continue;
+
+                        boolean isMineOwnerNeighbour = map[yOrdinateOfNeighbour][xOrdinateOfNeighbour].equals("*");
+                        if (isMineOwnerNeighbour) minesAround++;
+                    }
+
+                    mapReport[0][xOrdinate] = String.valueOf(minesAround);
+                }
+
+            }
+        }
+
+        for (int yOrdinate = 0; yOrdinate < MAP_HEIGH; yOrdinate++) {
+            for (int xOrdinate = 0; xOrdinate < MAP_WIDTH; xOrdinate++) {
+                String currentCellReport = mapReport[0][xOrdinate];
+                System.out.print(currentCellReport);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
